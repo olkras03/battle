0210629de92b2f4a86417e3db175617bb2aa2153
Lena Krasnova <olkras03@gmail.com>
2019-10-29 17:23:39 +0000

    test is passing

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,6 @@ feature "Enter names" do
     fill_in :player_1_name, with: "Dave"
     fill_in :player_2_name, with: "Mittens"
     click_button "Submit"
-    expect(page).to have_content "Dave vs. Mittens"
+    expect(page).to have_content("Dave vs. Mittens")
   end
 end
